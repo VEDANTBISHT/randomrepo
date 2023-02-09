10f2fd21044235ed7426482f586430dd5079a682
VEDANTBISHT <81436519+VEDANTBISHT@users.noreply.github.com>
2023-02-09 21:41:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//first we import the schema creator
+
+
+
+//import schema types from any plugins that might expose them
+
+
+//impoerted the testimonials.js file 
+import testimonials from './testimonials'
+
+
+export const schemaTypes = [testimonials]
+//then we give our schema to the builder and provide the result to the sanity
+export default createSchema({
+
+    //name of my schema
+    name: 'default',
+    //proceed to concatenate our document type
+    //to the ones provided by any plugins that are installed
+
+    type: 'schemaTypes.contact'([
+        testimonials
+
+    ]),
+})
---
+++ testimonials.js
@@ -0,0 +1,35 @@
+export default{
+    name: 'testimonisls',
+    title: 'testimonials',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+
+        },
+        {
+            name: 'company',
+            title: 'Copany',
+            type: 'string',
+
+        },
+        {
+            name: 'imageurl',
+            title: 'ImgURL',
+            type: 'image',
+            options: {
+                hotspot: true, //hotspot means the person can choose to crop the image while uploading//
+
+            }
+
+        },
+        {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string',
+
+        },
+    ],
+}
